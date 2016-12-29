3152d810f0dff096ac88305474bc159d637976b6
nickfu <net456@163.com>
2016-12-29 14:04:52 +0800

    添加公众号分享校验文件

---
+++ api.php
@@ -0,0 +1,130 @@
+<?php
+$S_KEY = 'zuinanji!@#123';
+
+//$k = 201612092025
+//$t = 84a6e8cb5859af63ae772aff66643070
+//默认值
+
+$k = $_GET['k'];
+$t = $_GET['t'];
+
+$check = false;
+
+if($t == md5($k.$S_KEY) && !empty($k)){
+	$check = true;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html class="VACT_body_page_text1"   >
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=320,minimum-scale=0.5, maximum-scale=5, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="   "/> <!--SEO -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>醉南鸡产品溯源查询系统</title>
+	
+	<link href="/api.css" rel="stylesheet" type="text/css"/> 
+    <style>
+	
+	</style>
+</head>
+<body class="VACT_mobile" style="overflow-x:hidden;border:0px;">
+
+
+
+<div class="VACT_main " >
+    <!-- <div class="VACT_grid"></div> -->
+    <!-- 头部 -->
+    <div class="VACT_headerWrapper_box">
+        <div id="VACT_headerWrapper" class="VACT_headerWrapper vAct_modexBox_header">
+            <div container="1" class="VACT_inner"><!-- vAct_modexBox_header --></div>
+        </div>
+    </div>
+    <!-- 主内容 -->
+    <div class="VACT_main_page_text1_box">
+        <div id="VACT_mainWrapper" class="VACT_mainWrapper vAct_modexBox_main VACT_main_page_text1">
+            <div container="1" class="VACT_inner">
+
+<div id="vAct_modexBox_image_1481272546013" class="VACT_modelBox&#x20;vAct_modexBox_image_1481272546013&#x20;" data-align="" style="position:absolute;width:127px;height:53px;left:9px;z-index:50000;top:13px;"><div class="v_image_style1&#x20;vAct_modexBox_image_1481272546013_css" style="width:127px;height:53px;"><img style="cursor:default;width:100%;height:100%;" src="http&#x3A;&#x2F;&#x2F;fl1.gtimg.com&#x2F;flpartition0&#x2F;409&#x2F;2001820409&#x2F;204&#x2F;571242170810704261504916572940.png&#x21;t254x106.png" id="imageId_584a6d92417d6" class="imageId_584a6d92417d6" data-scaling="scale"></img></div><!-- vAct_modexBox_image_1481272546013 --></div>
+
+<div id="vAct_modexBox_paragraph_148127248416807" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_148127248416807&#x20;" data-align="" style="position:absolute;width:146px;height:32px;left:61px;z-index:50001;top:729px;"><div style="width:146px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_148127248416807_css"><p><font color="#ff8c00"><span style="font-size: 16px; line-height: 32px;"><b>文昌鸡供应</b></span></font></p>
+</div><!-- vAct_modexBox_paragraph_148127248416807 --></div>
+
+<div id="vAct_modexBox_image_14812724841549" class="VACT_modelBox&#x20;vAct_modexBox_image_14812724841549&#x20;" data-align="" style="position:absolute;width:319px;height:107px;left:0px;z-index:50002;top:570px;"><div class="v_image_style1&#x20;vAct_modexBox_image_14812724841549_css" style="width:319px;height:107.994791667px;"><img style="cursor:default;width:100%" src="http&#x3A;&#x2F;&#x2F;fl2.gtimg.com&#x2F;flpartition0&#x2F;2112&#x2F;2000682112&#x2F;301&#x2F;9789624980201043022583157112131.jpg" id="imageId_584a76737eff7" class="imageId_584a76737eff7" data-scaling="flexible-left"></img></div><!-- vAct_modexBox_image_14812724841549 --></div>
+
+<div id="vAct_modexBox_paragraph_148127248455741" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_148127248455741&#x20;" data-align="" style="position:absolute;width:245px;height:63px;left:61px;z-index:50004;top:762px;"><div style="width:245px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_148127248455741_css"><p><font color="#696969"><span style="font-size: 13px;"><b>原产地直供，采用空运，快递，同城冷链等物流渠道，为餐厅，酒楼等餐饮场所专业批发文昌鸡</b></span></font></p>
+</div><!-- vAct_modexBox_paragraph_148127248455741 --></div>
+
+<div id="vAct_modexBox_paragraph_148127248436569" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_148127248436569&#x20;" data-align="" style="position:absolute;width:146px;height:32px;left:61px;z-index:50005;top:851px;"><div style="width:146px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_148127248436569_css"><p><font color="#ff8c00"><span style="font-size: 16px; line-height: 32px;"><b>文昌鸡菜品零售</b></span></font></p>
+</div><!-- vAct_modexBox_paragraph_148127248436569 --></div>
+
+<div id="vAct_modexBox_paragraph_148127248445559" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_148127248445559&#x20;" data-align="" style="position:absolute;width:245px;height:63px;left:61px;z-index:50006;top:889px;"><div style="width:245px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_148127248445559_css"><p><font color="#696969"><span style="font-size: 13px;"><b>拥有实体店，以及微信等在线销售渠道，顾客可以采取电话订购，微信下单等多种购买方式，深圳同城热食现做派送上门。</b></span></font></p>
+</div><!-- vAct_modexBox_paragraph_148127248445559 --></div>
+
+<div id="vAct_modexBox_paragraph_1481274795804" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_1481274795804&#x20;" data-align="" style="position:absolute;width:245px;height:63px;left:61px;z-index:50025;top:1032px;"><div style="width:245px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_1481274795804_css"><p><font color="#696969"><span style="font-size: 13px;"><b>研究各种标准化菜品，和挖掘多种供应链渠道，提供多种代理运营合作方式，可零成本加盟。</b></span></font></p>
+</div><!-- vAct_modexBox_paragraph_1481274795804 --></div>
+
+<div id="vAct_modexBox_paragraph_148127248445256" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_148127248445256&#x20;" data-align="" style="position:absolute;width:146px;height:32px;left:61px;z-index:50007;top:995px;"><div style="width:146px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_148127248445256_css"><p><font color="#ff8c00"><span style="font-size: 16px; line-height: 32px;"><b>代理合作加盟</b></span></font></p>
+</div><!-- vAct_modexBox_paragraph_148127248445256 --></div>
+
+<div id="vAct_modexBox_arrowLine_148127248422318" class="VACT_modelBox&#x20;vAct_modexBox_arrowLine_148127248422318&#x20;" data-align="" style="position:absolute;width:69px;height:30px;left:-2px;z-index:50012;transform:rotate(90deg);-webkit-transform:rotate(90deg);top:782px;"><div container="1" type="arrowLine" class="v_arrowLine_st&lt;x&gt;yle1&#x20;vAct_modexBox_arrowLine_148127248422318_css" style="width:69px;height:30px;"><div class="lineWrap"><div class="leftArrow"></div><div class="lineBox"><div class="line"></div></div><div class="rightArrow"></div></div></div><!-- vAct_modexBox_arrowLine_148127248422318 --></div>
+
+<div id="vAct_modexBox_arrowLine_148127248490535" class="VACT_modelBox&#x20;vAct_modexBox_arrowLine_148127248490535&#x20;" data-align="" style="position:absolute;width:81px;height:30px;left:-9px;z-index:50013;transform:rotate(90deg);-webkit-transform:rotate(90deg);top:908px;"><div container="1" type="arrowLine" class="v_arrowLine_st&lt;x&gt;yle1&#x20;vAct_modexBox_arrowLine_148127248490535_css" style="width:81px;height:30px;"><div class="lineWrap"><div class="leftArrow"></div><div class="lineBox"><div class="line"></div></div><div class="rightArrow"></div></div></div><!-- vAct_modexBox_arrowLine_148127248490535 --></div>
+
+<div id="vAct_modexBox_arrowLine_1481274758135" class="VACT_modelBox&#x20;vAct_modexBox_arrowLine_1481274758135&#x20;" data-align="" style="position:absolute;width:81px;height:30px;left:-9px;z-index:50023;transform:rotate(90deg);-webkit-transform:rotate(90deg);top:1046px;"><div container="1" type="arrowLine" class="v_arrowLine_st&lt;x&gt;yle1&#x20;vAct_modexBox_arrowLine_1481274758135_css" style="width:81px;height:30px;"><div class="lineWrap"><div class="leftArrow"></div><div class="lineBox"><div class="line"></div></div><div class="rightArrow"></div></div></div><!-- vAct_modexBox_arrowLine_1481274758135 --></div>
+
+<div id="vAct_modexBox_paragraph_1481272748950" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_1481272748950&#x20;" data-align="" style="position:absolute;width:131px;height:23px;left:176px;z-index:50015;top:25px;"><div style="width:131px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_1481272748950_css"><p><span style="font-size:20px;"><strong><span style="padding: 2px; word-break: normal;">产品查询系统</span></strong></span></p>
+</div><!-- vAct_modexBox_paragraph_1481272748950 --></div>
+
+<div id="vAct_modexBox_arrowLine_1481272779307" class="VACT_modelBox&#x20;vAct_modexBox_arrowLine_1481272779307&#x20;" data-align="" style="position:absolute;width:320px;height:30px;left:0px;z-index:50016;top:73px;"><div container="1" type="arrowLine" class="v_arrowLine_st&lt;x&gt;yle1&#x20;vAct_modexBox_arrowLine_1481272779307_css" style="width:320px;height:30px;"><div class="lineWrap"><div class="leftArrow"></div><div class="lineBox"><div class="line"></div></div><div class="rightArrow"></div></div></div><!-- vAct_modexBox_arrowLine_1481272779307 --></div>
+
+
+<?php if($check){?>
+<div id="vAct_modexBox_image_1481272968368" class="VACT_modelBox&#x20;vAct_modexBox_image_1481272968368&#x20;" data-align="" style="position:absolute;width:55px;height:60px;left:2px;z-index:50018;top:114px;"><div class="v_image_style1&#x20;vAct_modexBox_image_1481272968368_css" style="width:55px;height:60px;">
+<img style="cursor:default;width:100%;height:100%;" src="http&#x3A;&#x2F;&#x2F;fl5.gtimg.com&#x2F;flpartition0&#x2F;7088&#x2F;631007088&#x2F;762&#x2F;2754922528171269701821720695901.jpg&#x21;t110x120.jpg" id="imageId_584a70f463925" class="imageId_584a70f463925" data-scaling="scale"></img></div><!-- vAct_modexBox_image_1481272968368 --></div>
+<div id="vAct_modexBox_paragraph_1481273281129" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_1481273281129&#x20;" data-align="" style="position:absolute;width:247px;height:46px;left:68px;z-index:50019;top:123px;"><div style="width:247px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_1481273281129_css"><p><span style="font-size:20px;"><b>您查询的产品为醉南鸡经营的产品，感谢您的支持！</b></span></p>
+</div></div>
+<?php }else{?>
+<div id="vAct_modexBox_paragraph_1481273281129" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_1481273281129&#x20;" data-align="" style="position:absolute;width:247px;height:46px;left:68px;z-index:50019;top:123px;"><div style="width:247px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_1481273281129_css"><p><span style="font-size:20px;color:red"><b>该产品为非醉南鸡经营生产的，请联系我们！</b></span></p>
+</div></div>
+<?php }?>
+<div id="vAct_modexBox_paragraph_1481273756550" class="VACT_modelBox&#x20;vAct_modexBox_paragraph_1481273756550&#x20;" data-align="" style="position:absolute;width:310px;height:63px;left:5px;z-index:50020;top:467px;"><div style="width:310px;height:100%;word-break:normal;" class="VAct_urlCheck&#x20;v_paragraph_st&lt;x&gt;yle1&#x20;vAct_modexBox_paragraph_1481273756550_css"><p>深圳醉南鸡餐饮管理有限公司专注海南文昌鸡批发，菜品研发、文昌鸡食材供应。是致力于打造海南文昌鸡特色精品菜系的现代化餐饮管理公司。</p>
+</div><!-- vAct_modexBox_paragraph_1481273756550 --></div>
+
+<div id="vAct_modexBox_image_1481274570910" class="VACT_modelBox&#x20;vAct_modexBox_image_1481274570910&#x20;" data-align="" style="position:absolute;width:24px;height:24px;left:20px;z-index:50021;top:732px;"><div class="v_image_style1&#x20;vAct_modexBox_image_1481274570910_css" style="width:24px;height:24px;"><img style="cursor:default;width:100%;height:100%;" src="http&#x3A;&#x2F;&#x2F;fl1.gtimg.com&#x2F;flpartition0&#x2F;7088&#x2F;631007088&#x2F;635&#x2F;70409517502299557113831763401231.png&#x21;t48x48.png" id="imageId_584a767368c7f" class="imageId_584a767368c7f" data-scaling="scale"></img></div><!-- vAct_modexBox_image_1481274570910 --></div>
+
+<div id="vAct_modexBox_image_1481274682728" class="VACT_modelBox&#x20;vAct_modexBox_image_1481274682728&#x20;" data-align="" style="position:absolute;width:24px;height:24px;left:19px;z-index:50022;top:851px;"><div class="v_image_style1&#x20;vAct_modexBox_image_1481274682728_css" style="width:24px;height:24px;"><img style="cursor:default;width:100%;height:100%;" src="http&#x3A;&#x2F;&#x2F;fl1.gtimg.com&#x2F;flpartition0&#x2F;7088&#x2F;631007088&#x2F;635&#x2F;70409517502299557113831763401231.png&#x21;t48x48.png" id="imageId_584a76736b9ec" class="imageId_584a76736b9ec" data-scaling="scale"></img></div><!-- vAct_modexBox_image_1481274682728 --></div>
+
+<div id="vAct_modexBox_image_1481274773123" class="VACT_modelBox&#x20;vAct_modexBox_image_1481274773123&#x20;" data-align="" style="position:absolute;width:24px;height:24px;left:19px;z-index:50024;top:988px;"><div class="v_image_style1&#x20;vAct_modexBox_image_1481274773123_css" style="width:24px;height:24px;"><img style="cursor:default;width:100%;height:100%;" src="http&#x3A;&#x2F;&#x2F;fl1.gtimg.com&#x2F;flpartition0&#x2F;7088&#x2F;631007088&#x2F;635&#x2F;70409517502299557113831763401231.png&#x21;t48x48.png" id="imageId_584a767377188" class="imageId_584a767377188" data-scaling="scale"></img></div><!-- vAct_modexBox_image_1481274773123 --></div>
+
+<div id="vAct_modexBox_image_1481274844898" class="VACT_modelBox&#x20;vAct_modexBox_image_1481274844898&#x20;" data-align="" style="position:absolute;width:320px;height:189px;left:0px;z-index:50026;top:248px;"><div class="v_image_style1&#x20;vAct_modexBox_image_1481274844898_css" style="width:320px;height:189px;"><img style="cursor:default;width:100%;height:100%;" src="http&#x3A;&#x2F;&#x2F;fl4.gtimg.com&#x2F;flpartition0&#x2F;7088&#x2F;631007088&#x2F;642&#x2F;2142208841950297514791692610729.jpg" id="imageId_584a76737c1df" class="imageId_584a76737c1df" data-scaling="scale"></img></div><!-- vAct_modexBox_image_1481274844898 --></div>
+
+<div id="vAct_modexBox_arrowLine_1481274936606" class="VACT_modelBox&#x20;vAct_modexBox_arrowLine_1481274936606&#x20;" data-align="" style="position:absolute;width:320px;height:30px;left:-1px;z-index:50027;top:212px;"><div container="1" type="arrowLine" class="v_arrowLine_st&lt;x&gt;yle1&#x20;vAct_modexBox_arrowLine_1481274936606_css" style="width:320px;height:30px;"><div class="lineWrap"><div class="leftArrow"></div><div class="lineBox"><div class="line"></div></div><div class="rightArrow"></div></div></div><!-- vAct_modexBox_arrowLine_1481274936606 --></div>
+
+<div id="vAct_modexBox_arrowLine_1481274989751" class="VACT_modelBox&#x20;vAct_modexBox_arrowLine_1481274989751&#x20;" data-align="" style="position:absolute;width:320px;height:30px;left:0px;z-index:50028;top:1117px;"><div container="1" type="arrowLine" class="v_arrowLine_st&lt;x&gt;yle1&#x20;vAct_modexBox_arrowLine_1481274989751_css" style="width:320px;height:30px;"><div class="lineWrap"><div class="leftArrow"></div><div class="lineBox"><div class="line"></div></div><div class="rightArrow"></div></div></div><!-- vAct_modexBox_arrowLine_1481274989751 --></div>
+
+<div id="vAct_modexBox_button_1481275333987" class="VACT_modelBox&#x20;vAct_modexBox_button_1481275333987&#x20;" data-align="" style="position:absolute;width:256px;height:57px;left:29px;z-index:50034;top:1151px;"><button onclick="window.location.href='http://www.zuinanji.net'" id="buttonId_584a7aa17c92f" type="button" class="v_button_style5&#x20;vAct_modexBox_button_1481275333987_css" style="width:256px;height:57px;">访问官网</button><!-- vAct_modexBox_button_1481275333987 --></div>
+
+<div id="vAct_modexBox_button_1481275420054" class="VACT_modelBox&#x20;vAct_modexBox_button_1481275420054&#x20;" data-align="" style="position:absolute;width:256px;height:57px;left:30px;z-index:50035;top:1237px;"><button id="buttonId_584a7b388608f" type="button" class="v_button_style5&#x20;vAct_modexBox_button_1481275420054_css" style="width:256px;height:57px;">电话：18575521512</button><!-- vAct_modexBox_button_1481275420054 --></div>
+
+<div id="vAct_modexBox_phone_1481276158055" class="VACT_modelBox&#x20;vAct_modexBox_phone_1481276158055&#x20;" data-align="" style="position:absolute;width:185px;height:91px;left:50px;z-index:50040;top:1237px;"><div class="v_phone_style1&#x20;vAct_modexBox_phone_1481276158055_css" style="width:185px;height:91px;"><a href="tel:18575521512" style="width:185px;height:91px;position:absolute;top:0px;"><img src="http&#x3A;&#x2F;&#x2F;fl3.gtimg.com&#x2F;flpartition0&#x2F;2112&#x2F;2000682112&#x2F;711&#x2F;5247185112062076100611237101909.png&#x3F;rd&#x3D;6250164" alt="&#x62E8;&#x6253;&#x7535;&#x8BDD;" title="&#x62E8;&#x6253;&#x7535;&#x8BDD;" style="width:185px;height:91px;position:absolute;top:0px;"></img></a></div><!-- vAct_modexBox_phone_1481276158055 --></div></div>
+        </div>
+    </div>
+    <!-- 底部 -->
+    <div class="VACT_footererWrapper_box">
+        <div id="VACT_footererWrapper" class="VACT_footererWrapper vAct_modexBox_footer">
+            <div container="1" class="VACT_inner"><!-- vAct_modexBox_footer --></div>
+        </div>
+    </div>
+    <!-- 流式布局 -->
+    <div class="VACT_FlowlayoutWrapper"><!-- VACT_FlowlayoutWrapper --></div>
+</div>
+
+
+</body>
+</html>
+
